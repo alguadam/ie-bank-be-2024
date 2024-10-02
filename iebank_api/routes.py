f18ad0721beeb50285b30af9cef3086262c9609c
Anna Shats <ashats.ieu2022@student.ie.edu>
2024-10-02 14:24:38 +0200

    Chanches made to modules and routes for country

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -27,6 +27,8 @@ def create_account():
     name = request.json['name']
     currency = request.json['currency']
     account = Account(name, currency)
+    country = request.json['country']  # Get country from the request
+    account = Account(name, currency, country)  # Pass country 
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -63,5 +65,6 @@ def format_account(account):
         'balance': account.balance,
         'currency': account.currency,
         'status': account.status,
-        'created_at': account.created_at
+        'created_at': account.created_at,
+        'country': account.country  # Include the country 
     }
