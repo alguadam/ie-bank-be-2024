e918f7fb0faaf5869a2e77b1dc6e155dda397436
Zeid Tayyan <zaltayan.ieu2022@student.ie.edu>
2024-10-12 16:33:26 +0200

    added country and deployment

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
+from sqlalchemy import text
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
 
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'local':
@@ -21,6 +23,9 @@ db = SQLAlchemy(app)
 from iebank_api.models import Account
 
 with app.app_context():
+    query = text("ALTER TABLE account ADD COLUMN country VARCHAR(32)")
+    db.session.execute(query)
+    db.session.commit()
     db.create_all()
 CORS(app)
 
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -7,9 +7,10 @@ def test_create_account():
     WHEN a new Account is created
     THEN check the name, account_number, balance, currency, status and created_at fields are defined correctly
     """
-    account = Account('John Doe', '€')
-    assert account.name == 'John Doe'
+    account = Account('Zeid', '€', 'Jordan')
+    assert account.name == 'Zeid'
     assert account.currency == '€'
+    assert account.country == 'Jordan'
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
